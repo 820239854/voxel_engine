d2aaeedb6ee6f1a58130df0cd0e9f2919e58f41b
Xing <820239854@qq.com>
2023-01-03 20:56:18 +0800

    绘制像素点

--- source/i_voxel.c
+++ source/i_voxel.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <math.h>
 #include "dos.h"
@@ -5,14 +6,34 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 200
 
+uint8_t* heightmap = NULL;
+uint8_t* colormap = NULL;
+
 int main(int argc, char* args[]) {
     setvideomode(videomode_320x200);
+    
+    uint8_t palatte[256*3];
+    int map_width, map_height, pal_count;
+    colormap = loadgif("files/C1W.gif", &map_width, &map_height, &pal_count, palatte);
+    heightmap = loadgif("files/D1.gif", &map_width, &map_height, NULL, NULL);
+    
+    for(int i=0; i<pal_count; i++)
+    {
+        setpal(i, palatte[3*i + 0], palatte[3*i + 1], palatte[3*i + 2]);
+    }
+    setpal(0, 36, 36, 56);
+    
     setdoublebuffer(1);
     uint8_t* framebuffer = screenbuffer();
     
     while (!shuttingdown()) {
         waitvbl();
         clearscreen();
+        
+        int x = 160;
+        int y = 100;
+        
+        framebuffer[SCREEN_WIDTH * y + x] = 0x50;
         
         framebuffer = swapbuffers();
         
